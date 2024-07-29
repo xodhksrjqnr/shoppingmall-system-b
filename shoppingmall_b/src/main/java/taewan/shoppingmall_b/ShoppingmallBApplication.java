0553a0be36fb5f4a067a1f4bdb8ac1ab1d7ef577
xodhksrjqnr <xodhksrjqnr@naver.com>
2024-07-29 18:57:30 +0900

    project init

---
+++ shoppingmall_b/src/main/java/taewan/shoppingmall_b/ShoppingmallBApplication.java
@@ -0,0 +1,13 @@
+package taewan.shoppingmall_b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingmallBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingmallBApplication.class, args);
+	}
+
+}
---
+++ shoppingmall_b/src/test/java/taewan/shoppingmall_b/ShoppingmallBApplicationTests.java
@@ -0,0 +1,13 @@
+package taewan.shoppingmall_b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingmallBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
